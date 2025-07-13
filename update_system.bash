6b46cf63ab26fbba0723fcb915fd44e12bbe6309
Kasper Skytte Andersen <github@kasper.slmail.me>
2025-07-13 09:23:24 +0200

    initial system upgrade as ansible tasks

--- update_system.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#Update system (first run only)
-set -eux
-sudo apt-get update -y
-sudo apt-get upgrade -y --allow-downgrades
-sudo apt-get dist-upgrade -y
-sudo apt-get autoremove -y
-sudo apt-get autoclean -y
